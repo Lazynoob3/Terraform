424a1321845d75bacd6e9393ad5ba65eff49264d
ravilla-keerthi-naidu <r.k.naidu0377@gmail.com>
2024-08-01 20:29:44 +0530

    subnet, igw and rt added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,3 +4,24 @@ module "vpc" {
   cidr_block = "10.0.0.0/16"
   name       = "dev-vpc"
 }
+
+module "internet_gateway" {
+  source = "../../modules/internet_gateway"
+  vpc_id = module.vpc.vpc_id
+  name   = "igw-dev-vpc"
+}
+
+module "subnets" {
+  source = "../../modules/subnets"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+module "route_tables" {
+  source = "../../modules/route_tables"
+  vpc_id = module.vpc.vpc_id
+  internet_gateway_id = module.internet_gateway.internet_gateway_id
+  public_subnet_ids   = module.subnets.public_subnets
+  private_subnet_ids  = module.subnets.private_subnets
+}
