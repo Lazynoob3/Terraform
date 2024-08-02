9549d59fb415e57114f7c6b3d0893ea570a31f27
ravilla-keerthi-naidu <r.k.naidu0377@gmail.com>
2024-08-02 10:09:49 +0530

    deleted all resources

--- modules/subnets/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_subnets" {
-  description = "The public subnets IDs"
-  value       = aws_subnet.public.*.id
-}
-
-output "private_subnets" {
-  description = "The private subnets IDs"
-  value       = aws_subnet.private.*.id
-}
--- modules/vpc/main.tf
+++
@@ -1,8 +0,0 @@
-# modules/vpc/main.tf
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-
-  tags = {
-    Name = var.name
-  }
-}
